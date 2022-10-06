6b01c73bd7c43f9e0998241353cec24d7d9554c7
VichiesUA <stasyatsyn@gmail.com>
2022-10-06 11:03:55 +0300

    Push final version1

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,10 +1,11 @@
 from faker import Faker
 from django.http import HttpRequest, HttpResponse
+from apps.base.services.get_random_user import get_random_user
 
 faker = Faker()
 
 
 def index(request: HttpRequest, name: str | None = None) -> HttpResponse:
     if name is None:
-        name = faker.first_name()
-    return HttpResponse(f"Hello,  {name}")
+        name = get_random_user()
+    return HttpResponse(f"Hello, {name}")
